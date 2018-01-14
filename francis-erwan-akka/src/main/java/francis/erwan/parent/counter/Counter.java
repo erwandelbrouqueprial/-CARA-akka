85094e216c4f4ad12e02a4fd9875337e8ab5098d
erwan <e.delbrouque-prial@etudiant.univ-lille1.fr>
2018-01-14 22:34:57 +0100

    Implémentation de la classe Counter qui permet de compter le nombre d'occurences lettres ou mots d'un texte

---
+++ francis-erwan-akka/src/main/java/francis/erwan/parent/counter/Counter.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package francis.erwan.parent.counter;
+
+import java.util.HashMap;
+
+/**
+ * @author erwan
+ *
+ */
+public class Counter {
+
+	/**
+	 * HashMap ou la clé et une lettre, et la valeur son nombre d'occurences dans le texte
+	 */
+	private HashMap<Character, Integer> counterChar = new HashMap<>();
+	/**
+	 * HashMap ou la clé et un mot, et la valeur son nombre d'occurences dans le texte
+	 */
+	private HashMap<String, Integer> counterWords = new HashMap<>();
+
+	public Counter() {
+	}
+	
+	public HashMap<Character,Integer> charCounter(final String texte){
+		for (Character c : texte.toCharArray()) {
+			if (!counterChar.containsKey(c)) {
+				counterChar.put(c, 1);
+			} else {
+				counterChar.put(c, counterChar.get(c) + 1);
+			}
+		}
+
+		return counterChar;
+	}
+	
+	public HashMap<String,Integer> wordCounter(final String texte){
+		String[] words = texte.split(" ");
+		for (String c : words) {
+			if (!counterWords.containsKey(c)) {
+				counterWords.put(c, 1);
+			} else {
+				counterWords.put(c, counterWords.get(c) + 1);
+			}
+		}
+
+		return counterWords;
+	}
+	
+	
+
+	/**
+	 * @return the counterWords
+	 */
+	public HashMap<String, Integer> getCounterWords() {
+		return counterWords;
+	}
+
+	/**
+	 * @param counterWords the counterWords to set
+	 */
+	public void setCounterWords(HashMap<String, Integer> counterWords) {
+		this.counterWords = counterWords;
+	}
+
+	/**
+	 * @return the counterLetter
+	 */
+	public HashMap<Character, Integer> getCounterLetter() {
+		return counterChar;
+	}
+
+	/**
+	 * @param counterLetter
+	 *            the counterLetter to set
+	 */
+	public void setCounterLetter(HashMap<Character, Integer> counterLetter) {
+		this.counterChar = counterLetter;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Counter [counterChar=" + counterChar + ", counterWords=" + counterWords + "]";
+	}
+
+}
